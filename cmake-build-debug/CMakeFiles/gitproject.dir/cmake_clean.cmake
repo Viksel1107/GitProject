76f501f06c0edd7f0948ad443d078121b0d5ca5c
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2023-11-19 14:32:00 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/gitproject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitproject.dir/main.cpp.o"
+  "gitproject"
+  "gitproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gitproject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gitproject.
+# This may be replaced when dependencies are built.
